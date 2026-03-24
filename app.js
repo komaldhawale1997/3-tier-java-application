9ca1af08bbcde39e868c50b872d7f29e66840cef
KomalDhawale_Personal_Repo <komal250897@gmail.com>
2026-03-24 19:27:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const mysql = require('mysql2');
+app/src/main/java/com/example/App.java
+package com.example;
 
-const app = express();
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+public class App {
+    public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://" + System.getenv("DB_HOST") + ":3306/" + System.getenv("DB_NAME");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASSWORD");
 
-app.get("/", (req, res) => {
-  db.query("SELECT 1 + 1 AS result", (err, results) => {
-    if (err) return res.send("DB Error");
-    res.send("DB Connected: " + results[0].result);
-  });
-});
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("✅ Connected to MySQL DB successfully!");
 
-app.listen(5000, () => console.log("Backend running"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
